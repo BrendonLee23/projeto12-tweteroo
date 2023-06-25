1d6930d98b193bea8202f9994dd1b2d405862e5d
Brendo <brendow_prime@hotmail.com>
2023-06-25 14:36:46 -0400

    get tweets finalizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 
-const loginInfos=[];
+const loginInfos = [];
+const arrayTweets = [];
+const lastArray = [];
 
 const app = express();
 app.use(express.json());
@@ -20,10 +22,47 @@ app.post('/sign-up', (req, res) => {
     res.send("OK")
 })
 
+app.post('/tweets', (req, res) => {
+    const { username, tweet } = req.body
+
+    /*     const userExists = loginInfos.some(user => user.username === username); */
+
+    if (loginInfos.includes(username)) {
+        arrayTweets.push(
+            {
+                username: username,
+                tweet: tweet
+            }
+        )
+        res.send('OK');
+    } else {
+        res.send('UNAUTHORIZED');
+    }
+})
+
 app.get('/tweets', (req, res) => {
-    const tweets = []
-    res.send(loginInfos)
+    const { tweet } = req.body;
+    lastArray.push(
+        [
+            {
+                username: tweet.username,
+                avatar: tweet.avatar,
+                tweet: tweet.tweet
+            }
+        ]
+    )
+
+    if (lastArray.length > 10) {
+        // a função slice retorna os últimos 10 elementos
+        const lastTenTweets = lastArray.slice(-10);
+        res.json(lastTenTweets);
+    } else if(lastArray.length < 10){
+        // Caso tenha menos de 10 elementos, retorne o lastArray completo
+        res.json(lastArray);
+    } else{
+        res.json([])
+    }
 })
 
 const PORT = 5000
-app.listen(PORT, ()=> console.log(`O servidor está rodando na porta ${PORT}`))
+app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}`))
